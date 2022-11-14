f63b17f074ad279351e6dc9a3fe5397c6d709432
Iengem <gaadiiengem@gmail.com>
2022-11-14 12:26:10 +0100

    Modified with alerts

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 let butn = document.querySelector('#button');
 butn.addEventListener('click', function(){
     let eggs = document.querySelector('#eggs').value;
-    if(eggs > 1){
+    if(eggs > 100){
+        document.querySelector('#button').value = 'Too many eggs!!';
+    }
+    if(eggs > 1 && eggs < 101){
         document.querySelector('#button').value = 'You just bought '+ eggs +' eggs' ;
+        alert(eggs +' eggs successfully purchased.')
     }
     else if(eggs==1){
         document.querySelector('#button').value = 'You just bought ' + eggs + ' egg';
