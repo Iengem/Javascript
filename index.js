a0d0336cc41b54be64db50c2883836a4df5ee66d
Iengem <gaadiiengem@gmail.com>
2022-11-14 13:20:55 +0100

    Keyboard

---
+++ index.js
@@ -0,0 +1,144 @@
+keys = document.querySelector('.keys1');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += '1';
+})
+keys = document.querySelector('.keys2');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += '2';
+})
+keys = document.querySelector('.keys3');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += '3';
+})
+keys = document.querySelector('.keys4');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += '4';
+})
+keys = document.querySelector('.keys5');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += '5';
+})
+keys = document.querySelector('.keys6');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += '6';
+})
+keys = document.querySelector('.keys7');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += '7';
+})
+keys = document.querySelector('.keys8');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += '8';
+})
+keys = document.querySelector('.keys9');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += '9';
+})
+keys = document.querySelector('.keys10');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += '10';
+})
+keys = document.querySelector('.keys11');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'Q';
+})
+keys = document.querySelector('.keys12');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'W';
+})
+keys = document.querySelector('.keys13');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'E';
+})
+keys = document.querySelector('.keys14');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'R';
+})
+keys = document.querySelector('.keys15');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'T';
+})
+keys = document.querySelector('.keys16');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'Y';
+})
+keys = document.querySelector('.keys17');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'U';
+})
+keys = document.querySelector('.keys18');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'I';
+})
+keys = document.querySelector('.keys19');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'O';
+})
+keys = document.querySelector('.keys20');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'P';
+})
+keys = document.querySelector('.keys21');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'A';
+})
+keys = document.querySelector('.keys22');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'S';
+})
+keys = document.querySelector('.keys23');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'D';
+})
+keys = document.querySelector('.keys24');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'F';
+})
+keys = document.querySelector('.keys25');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'G';
+})
+keys = document.querySelector('.keys26');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'H';
+})
+keys = document.querySelector('.keys27');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'J';
+})
+keys = document.querySelector('.keys28');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'K';
+})
+keys = document.querySelector('.keys29');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'L';
+})
+keys = document.querySelector('.keys30');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'Z';
+})
+keys = document.querySelector('.keys31');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'X';
+})
+keys = document.querySelector('.keys32');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'C';
+})
+keys = document.querySelector('.keys33');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'V';
+})
+keys = document.querySelector('.keys34');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'B';
+})
+keys = document.querySelector('.keys35');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'N';
+})
+keys = document.querySelector('.keys36');
+keys.addEventListener('click', function(){
+    document.querySelector('#bar').value += 'M';
+})
