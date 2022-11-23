d11e86b31dc21767c467f6861e822131f735c4f9
Iengem <gaadiiengem@gmail.com>
2022-11-23 18:56:20 +0100

    refactored

--- arrays.js
+++ arrays.js
@@ -1,22 +1,16 @@
-// // function that prints lower than target
-// function lower(arr, target){
-//     value= []
-//     for(i=0;i<arr.length; i++){
-//         value = arr[i]
-//         if(value < target){
-//             console.log(value);
-//         }
-//         break;
-//     }
-//     if(value>1){
-//     return 'These values are lower than '+ target;
-//     }
-//     else{
-//         return'This value is lower than '+ target;
-//     }
-
-// }
-// console.log(lower([1,2,3,4,5],2))
+// function that prints lower than target
+function lower(arr, target){
+    arr = arr.sort((a,b)=>a-b)
+    value= []
+    for(i=0;i<arr.length; i++){
+        value = arr[i]
+        if(value < target){
+            console.log(value)
+        }
+    }
+    return 'These values in the array are less than ' + target;
+}
+console.log(lower([80,20,70,45,5], 100))
 
 // function that prints index of numbers whose sum === target
 function index(arr, target){
@@ -35,4 +29,4 @@ function index(arr, target){
     }
 }
 }
-console.log(index([1, 2, 3, 4], 7))
+// console.log(index([1, 2, 3, 4], 7))
